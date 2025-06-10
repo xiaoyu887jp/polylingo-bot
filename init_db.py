1739d7cca46a45969619e211e58541ebc1892c75
xiaoyu887jp <xiaoyu887jp@gmail.com>
2025-06-10 13:42:42 +0900

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -2,19 +2,18 @@ import sqlite3
 
 DATABASE = 'data.db'
 
-def create_group_settings_table():
+def create_user_quota_table():
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS group_settings (
-            group_id TEXT PRIMARY KEY,
-            card_sent INTEGER DEFAULT 0
+        CREATE TABLE IF NOT EXISTS user_quota (
+            user_id TEXT PRIMARY KEY,
+            quota INTEGER DEFAULT 2000
         )
     ''')
     conn.commit()
     conn.close()
 
 if __name__ == "__main__":
-    create_group_settings_table()
-    print("group_settings 表已创建或已存在。")
-
+    create_user_quota_table()
+    print("user_quota 表已创建或已存在。")
